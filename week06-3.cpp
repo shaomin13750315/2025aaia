f67b4bf88a82bd067bbd09a84699162340cecbd1
shaomin13750315 <13750315@me.mcu.edu.tw>
2025-03-28 12:00:13 +0800

    week06

---
+++ week06-3.cpp
@@ -0,0 +1,12 @@
+///week06-3.cpp
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while(n>0){
+            if(n%3==2)return false;
+            n = n / 3;
+
+        }
+        return true;
+    }
+};
